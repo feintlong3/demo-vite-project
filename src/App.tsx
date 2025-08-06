742826bafbf2f3786e2449540a79c0796710c193
feintlong3 <feintlong3@gmail.com>
2025-08-07 03:46:43 +0900

    improve typecheck

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 
 import reactLogo from './assets/react.svg'
+import { add } from './utils/add'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>{add(1, 3)}</p>
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
